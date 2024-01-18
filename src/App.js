83a300e35201e9619492b7e935f1acff14a701dc
aim9886 <aimanth9886@gmail.com>
2024-01-18 13:54:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import FoundPage from "./pages/FoundPage";
+import LostPage from "./pages/LostPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screen">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="lost" element={<LostPage />} />
+          <Route path="found" element={<FoundPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
